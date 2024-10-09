544567a71b91c468abd7b5ddc97f5565df5112e4
Will Lumley <will@lumley.io>
2024-10-09 18:26:13 +1100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "SwiftLlama", targets: ["SwiftLlama"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/will-lumley/llama.cpp.git", from: "1.0.0")
+        .package(url: "https://github.com/will-lumley/llama.cpp.git", exact: "1.0.0")
     ],
     targets: [
         .target(name: "SwiftLlama", 
