9c9e113bd749f24a5fd1d8fca6c72a3fb87c58fb
Tim Wang <tim.wang@ing.com>
2025-04-19 08:18:08 +1000

    Use forked version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "SwiftLlama", targets: ["SwiftLlama"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ggerganov/llama.cpp.git", branch: "master")
+        .package(url: "https://github.com/ShenghaiWang/llama.cpp.git", branch: "master")
     ],
     targets: [
         .target(name: "SwiftLlama", 
